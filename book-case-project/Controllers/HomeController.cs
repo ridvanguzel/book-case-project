45cee823d5ce9bf9694ca7e3de56b1f2ee4e9a3c
ridvanguzel <ridvanguzel42@gmail.com>
2024-02-05 19:12:07 +0300

    Add project files.

---
+++ book-case-project/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using book_case_project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace book_case_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            var ktp = new List<Books>()
+            {
+                new Books(){ID = 1, BookName="Gör Beni", Writer="Azra Kohen"},
+                new Books(){ID = 2, BookName="Oysa Herkes Kendisiyle Meşgul", Writer="Murat Gülsoy"},
+                new Books(){ID = 3, BookName = "Asansör", Writer="Beyza Alkoç"},
+                new Books(){ID = 4, BookName = "Hayallerime Dokun", Writer="Edith Wharton"},
+                new Books(){ID = 5, BookName = "Allah'ın Güzel İsimleri", Writer="Özkan Öze"},
+            };
+
+            return View(ktp);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ book-case-project/Models/Books.cs
@@ -0,0 +1,9 @@
+﻿namespace book_case_project.Models
+{
+    public class Books
+    {
+        public int ID { get; set; }
+        public string BookName { get; set; }
+        public string Writer { get; set; }
+    }
+}
